cb9944650940e30f69838c628bddf8cd0710ff04
Shihab0 <ataharshihab1@gmail.com>
2023-12-02 02:32:52 +0600

    cross button added

--- src/Components/HamburgerMenu/HamburgerMenu.js
+++ src/Components/HamburgerMenu/HamburgerMenu.js
@@ -2,11 +2,15 @@ import React from "react";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
 import Icon0 from "../../images/icon0.png";
+import { IoCloseSharp } from "react-icons/io5";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
+  };
+  const closeDrawer = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -14,12 +18,24 @@ const HamburgerMenu = () => {
       <img src={Icon0} alt="" onClick={toggleDrawer} />
 
       <Drawer
+        style={{ backgroundColor: "#f0f0f0" }}
         open={isOpen}
         onClose={toggleDrawer}
         direction="left"
+        size={250}
         className="bla bla bla"
       >
         {/* inline css applicable */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "end",
+            padding: "20px",
+            fontSize: "30px",
+          }}
+        >
+          <IoCloseSharp onClick={closeDrawer} />
+        </div>
         <div>Hello World</div>
         <div>Hello World</div>
         <div>Hello World</div>
--- src/Components/HamburgerMenu/MobileHamburger.js
+++ src/Components/HamburgerMenu/MobileHamburger.js
@@ -2,11 +2,15 @@ import React from "react";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
 import MbIcon5 from "../../images/mb5.png";
+import { IoCloseSharp } from "react-icons/io5";
 
 const MobileHamburger = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
+  };
+  const closeDrawer = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -17,15 +21,26 @@ const MobileHamburger = () => {
         open={isOpen}
         onClose={toggleDrawer}
         direction="bottom"
+        size={500}
         className="bla bla bla"
       >
         {/* inline css applicable */}
-        <div style={{ margin: "20px auto" }}>
-          <div>Hello World</div>
-          <div>Hello World</div>
-          <div>Hello World</div>
-          <div>Hello World</div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "end",
+            padding: "20px",
+            fontSize: "30px",
+          }}
+        >
+          <IoCloseSharp onClick={closeDrawer} style={{ cursor: "pointer" }} />
         </div>
+
+        <div>Hello World</div>
+        <div>Hello World</div>
+        <div>Hello World</div>
+        <div>Hello World</div>
+
         {/* 
         code goes here */}
       </Drawer>
